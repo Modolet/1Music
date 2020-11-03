87dadfb1693e502cebc51bf3a868a05fd62e5e4d
modolet <modolet@163.com>
2020-11-03 10:28:04 +0800

    定义Player ListModel SongMedel

--- models.h
+++ models.h
@@ -2,15 +2,64 @@
 #define MODELS_H
 
 #include <QObject>
+#include <QString>
+#include <QDateTime>
+#include <QVector>
+#include <QJsonDocument>
+#include <QJsonValue>
+#include <QJsonArray>
 
 //音乐资源模型
 class Models : public QObject
 {
     Q_OBJECT
+    //歌曲模型
 public:
     explicit Models(QObject *parent = nullptr);
 
 signals:
+
+};
+
+class SongModel
+{
+public:
+    bool IsLocalSong;//是否为本地文件
+    int Id;//Id值。本地文件不需要id
+    QString Source;//音源
+    QString Url;//
+    QString Name;//歌名
+    QString Singer;//歌手
+    QString Album;//专辑
+
+    SongModel(QString url);
+};
+
+class ListModel
+{
+private:
+
+    void _setName();
+    void _setCreateTime();
+    void _setInfo();
+    void _updateFile();
+
+public:
+    ListModel(QString url);
+
+    bool IsLocalList;//是否为本地歌单
+    QString Url;//
+    QString Name;//歌单名
+    QString Info;//歌单简介/信息
+    QDateTime CreateTime;//创建时间
+    QString Author;//创建者
+    QVector<SongModel> Songs;//歌曲列表
+
+    void setName(QString name);
+    void setCreateTime(QDateTime time);
+    void insertSong(SongModel song);
+    void deleteSong(SongModel song);
+    void setInfo(QString info);
 
 };
 
--- player.h
+++ player.h
@@ -2,13 +2,37 @@
 #define PLAYER_H
 
 #include <QObject>
+#include <QString>
+#include "models.h"
 
 //播放器
 class Player : public QObject
 {
     Q_OBJECT
 public:
+    //无参构造
     explicit Player(QObject *parent = nullptr);
+    //有参构造
+    Player(QObject *parent = nullptr,ListModel *list = nullptr);
+    //获取播放器信息
+    //获取当前歌曲
+    QString getCurrentSong();
+    //获取歌单信息
+    QString getCurrentList();
+    //获取音量
+    int getVolume();
+    //设置音量
+    int setVolume(int value);
+    //播放
+    void play(QString url,bool video = false);//url为网络链接或者本地文件，video为是否播放视频
+    //暂停
+    void pause();
+    //停止
+    void stop();
+    //加载并播放指定歌曲
+    void loadSong(SongModel song);
+
+
 
 signals:
 
